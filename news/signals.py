69a905d237668d55acdbd16609b6f1c990cb0add
Despect <el-despecto@ya.ru>
2022-03-20 23:09:57 +0900

    D6.5
    D6.5 Периодические задачи

--- news/signals.py
+++ news/signals.py
@@ -7,16 +7,8 @@ from django.template.loader import render_to_string
 from .models import Post, Category
 
 
-# создаём функцию обработчик с параметрами под регистрацию сигнала
-# запускает выполнение кода при каком-либо действии пользователя, в нашем случае -
-# сохранение в БД модели Post записи
 @receiver(post_save, sender=Post)
 def send_sub_mail(sender, instance, created, **kwargs):
-    print()
-    print()
-    print('====================ПРОВЕРКА СИГНАЛОВ===========================')
-    print()
-    print('задача - отправка письма подписчикам при добавлении новой статьи')
 
     global subscriber
     sub_text = instance.text
@@ -28,37 +20,28 @@ def send_sub_mail(sender, instance, created, **kwargs):
 
     post = instance
 
-    # для удобства вывода инфы в консоль, никакой важной функции не несет
     print('Адреса рассылки:')
-    for qaz in subscribers:
-        print(qaz.email)
+    for msg in subscribers:
+        print(msg.email)
 
-    print()
     print()
     print()
     for subscriber in subscribers:
-        # для удобства вывода инфы в консоль, никакой важной функции не несет
-        print('**********************', subscriber.email, '**********************')
-        print()
-        print('Адресат:', subscriber.email)
+        print(subscriber.email)
 
         html_content = render_to_string(
-            'mail.html', {'user': subscriber, 'text': sub_text[:50], 'post': post})
+            'message_send.html', {'user': subscriber, 'text': sub_text[:50], 'post': post})
 
         msg = EmailMultiAlternatives(
-            subject=f'Здравствуй, {subscriber.username}. Новая статья в вашем разделе!',
-            from_email='factoryskill@yandex.ru',
+            subject=f'Здравствуйте, {subscriber.username}. Новая статья в вашем разделе!',
+            from_email='d3spector@yandex.ru',
             to=[subscriber.email]
         )
 
         msg.attach_alternative(html_content, 'text/html')
-
-        # для удобства вывода инфы в консоль, никакой важной функции не несет
         print()
         print(html_content)
         print()
-
-        # код ниже временно заблокирован, чтоб пока в процессе отладки не производилась реальная рассылка писем
-        # msg.send()
+        msg.send()
 
     return redirect('/news/')
